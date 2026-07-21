7a511a523dfacea9957a6a7b5d732aea3743dcc8
mifidon96 <86232639+mifidon96@users.noreply.github.com>
2026-07-21 15:15:28 +0100

    Create versions.tf for Terraform setup
    Add Terraform configuration with required version and providers.

---
+++ versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    aws    = { source = "hashicorp/aws", version = "~> 5.60" }
+    random = { source = "hashicorp/random", version = "~> 3.6" }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2" # London
+}
