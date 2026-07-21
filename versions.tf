610c82c2b60ece672d6ec84f9225da7574a0d5c4
Morgan Ifidon <Morgan.ifidon@googlemail.com>
2026-07-21 21:18:47 +0100

    Step 1: configure S3 backend with DynamoDB locking

--- versions.tf
+++ versions.tf
@@ -4,8 +4,15 @@ terraform {
     aws    = { source = "hashicorp/aws", version = "~> 5.60" }
     random = { source = "hashicorp/random", version = "~> 3.6" }
   }
+  backend "s3" {
+    bucket         = "morgan-tfstate-cicd-demo"
+    key            = "cicd-demo/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "morgan-tf-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
-  region = "eu-west-2" # London
+  region = "eu-west-2"
 }
